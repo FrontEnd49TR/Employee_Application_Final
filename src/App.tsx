720c1fe8e293336d127655d50162e3c34b7b7ed7
Eugene Kuzenko <yevgenkuzenko@gmail.com>
2023-03-19 18:29:54 +0200

    5_Firebase Authentication Authorization

--- src/App.tsx
+++ src/App.tsx
@@ -20,36 +20,40 @@ import { NavigatorDispatch } from './components/navigators/NavigatorDispatch';
 function App() {
     const dispatch = useDispatch<any>();
     const [routes, setRoutes] = useState<RouteType[]>([]);
-    const authUser: string = useSelector<any, string>(state => state.auth.authenticated);
-    useEffect(() => {
+    const authUser:string = useSelector<any,string>(state=>state.auth.authenticated );
+    useEffect(()=> {
         function getRoutes(): RouteType[] {
-            const logoutRoute: RouteType | undefined = layoutConfig.routes
-                .find(r => r.path.includes('logout'))
+            const logoutRoute: RouteType |undefined = layoutConfig.routes
+            .find(r => r.path.includes('logout'))
             logoutRoute!.label = authUser;
             return layoutConfig.routes.filter(r => (!authUser && !r.flAuth) ||
-                (authUser.includes('admin') && r.flAdmin) ||
-                (authUser && r.flAuth && !r.flAdmin))
+            (authUser.includes('admin') && r.flAdmin) ||
+            (authUser && r.flAuth && !r.flAdmin))
         }
         setRoutes(getRoutes());
     }, [authUser]);
     useEffect(() => {
-        dispatch(employeesActions.getEmployees());
-    }, [])
-    return <BrowserRouter>
-        <Routes>
-            <Route path='/' element={<NavigatorDispatch routes={routes} />}>
-                <Route index element={<Employees />} />
-                <Route path='add' element={<AddEmployee />} />
-                <Route path='statistics/age' element={<AgeStatistics />} />
-                <Route path='statistics/salary' element={<SalaryStatistics />} />
-                <Route path='login' element={<Login />} />
-                <Route path='logout' element={<Logout />} />
-                <Route path='generation' element={<Generation />} />
-
-            </Route>
-
-        </Routes>
-    </BrowserRouter>
+        if(authUser) {
+              dispatch(employeesActions.getEmployees());
+        }
+      
+    },[authUser])
+  return <BrowserRouter>
+      <Routes>
+          <Route path='/' element={<NavigatorDispatch 
+           routes={routes}  />}>
+              <Route index element={<Employees/>}/>
+              <Route path='add' element={<AddEmployee/>}/>
+              <Route path='statistics/age' element={<AgeStatistics/>}/>
+              <Route path='statistics/salary' element={<SalaryStatistics/>}/>
+              <Route path='login' element={<Login/>}/>
+              <Route path='logout' element={<Logout/>}/>
+              <Route path='generation' element={<Generation/>}/>
+              
+          </Route>
+              
+      </Routes>
+  </BrowserRouter>
 
 }
 export default App;
--- src/components/forms/LoginForm.tsx
+++ src/components/forms/LoginForm.tsx
@@ -12,97 +12,94 @@ import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import { LoginData } from '../../model/LoginData';
 import { Alert } from '@mui/material';
+import { CodeType } from '../../model/CodeType';
 
 function Copyright(props: any) {
-    return (
-        <Typography variant="body2" color="text.secondary" align="center" {...props}>
-            {'Copyright © '}
-            <Link color="inherit" href="https://tel-ran.com">
-
-                tel-ran.com
-
-            </Link>{' , '}
-            Eugene Kuzenko {' '}
-            {new Date().getFullYear()}
-            {'.'}
-        </Typography>
-    );
+  return (
+    <Typography variant="body2" color="text.secondary" align="center" {...props}>
+      {'Copyright © '}
+      <Link color="inherit" href="https://tel-ran.com">
+      tel-ran.com
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
 }
 type Props = {
-    submitFn: (loginData: LoginData) => string
+  submitFn: (loginData: LoginData)=>void;
+  code: CodeType;
 };
 const theme = createTheme();
 
-export const LoginForm: React.FC<Props> = ({ submitFn }) => {
-    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-        event.preventDefault();
-        const data = new FormData(event.currentTarget);
-        const loginData: LoginData = {
-            username: data.get("username") as string,
-            password: data.get("password") as string
-        }
-        setMessage(submitFn(loginData));
-    };
-    const [message, setMessage] = React.useState('');
-    return (
-        <ThemeProvider theme={theme}>
-            <Container component="main" maxWidth="xs">
-                <CssBaseline />
-                <Box
-                    sx={{
-                        marginTop: 8,
-                        display: 'flex',
-                        flexDirection: 'column',
-                        alignItems: 'center',
-                    }}
-                >
-                    <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-                        <LockOutlinedIcon />
-                    </Avatar>
-                    <Typography component="h1" variant="h5">
-                        Sign in
-                    </Typography>
-                    <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
-                        <TextField
-                            margin="normal"
-                            required
-                            fullWidth
-                            id="email"
-                            label="Username"
-                            name="username"
-                            autoComplete="email"
-                            autoFocus
-                        />
-                        <TextField
-                            margin="normal"
-                            required
-                            fullWidth
-                            name="password"
-                            label="Password"
-                            type="password"
-                            id="password"
-                            autoComplete="current-password"
-                        />
-
-                        <Button
-                            type="submit"
-                            fullWidth
-                            variant="contained"
-                            sx={{ mt: 3, mb: 2 }}
-                        >
-                            Sign In
-                        </Button>
-                        <Grid container>
-
-                            <Grid item>
-                                {message && <Alert severity='error' onClose={() => setMessage('')}>{message}</Alert>}
-                            </Grid>
-                        </Grid>
-                    </Box>
-                </Box>
-                <Copyright sx={{ mt: 8, mb: 4 }} />
-            </Container>
-
-        </ThemeProvider>
-    );
+export const LoginForm: React.FC<Props> = ({submitFn, code}) => {
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    const loginData: LoginData = {username: data.get("username") as string,
+  password: data.get("password") as string}
+ submitFn(loginData);
+  };
+  
+  return (
+    <ThemeProvider theme={theme}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign in
+          </Typography>
+          <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Username"
+              name="username"
+              autoComplete="email"
+              autoFocus
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+            />
+          
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Sign In
+            </Button>
+            <Grid container>
+             
+              <Grid item>
+              {code == 'Credentials Error' && <Alert severity='error' >{code}, enter another credentials</Alert>}
+              </Grid>
+            </Grid>
+          </Box>
+        </Box>
+        <Copyright sx={{ mt: 8, mb: 4 }} />
+      </Container>
+      
+    </ThemeProvider>
+  );
 }
